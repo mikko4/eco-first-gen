cc6629986b5de7917ba21801b82431fc9d6e8fd6
mikko4 <mikkotripakis@gmail.com>
2022-11-13 03:17:03 -0500

    add all files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import WalletInfo from "./WalletInfo";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WalletInfo />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
